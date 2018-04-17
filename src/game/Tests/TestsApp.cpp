0aa53be9df908ec6e441a632e452f3c1ca2f7035
Shamyl Zakariya <shamyl@gmail.com>
2018-04-17 09:19:02 -0700

    Make TestsApp support iterating through all tests

--- src/game/Tests/TestsApp.cpp
+++ src/game/Tests/TestsApp.cpp
@@ -16,25 +16,70 @@
 #include "EasingTestScenario.hpp"
 #include "ParticleSystemTestScenario.hpp"
 
-#define SCENARIO ParticleSystemTestScenario
-
 class TestsApp : public core::App {
 public:
 
     static void prepareSettings(Settings *settings) {
         App::prepareSettings(settings);
-        settings->setTitle("Tests");
     }
 
 public:
 
-    TestsApp() {
+    TestsApp():
+            _index(0) {
     }
 
-    virtual void setup() override {
+    void setup() override {
         App::setup();
-        setScenario(make_shared<SCENARIO>());
+        
+        _scenarioFactories = {
+            [](){ return make_pair("ParticleSystemTestScenario", make_shared<ParticleSystemTestScenario>()); },
+            [](){ return make_pair("PerlinWorldTestScenario", make_shared<PerlinWorldTestScenario>()); },
+            [](){ return make_pair("SvgTestScenario", make_shared<SvgTestScenario>()); },
+            [](){ return make_pair("TerrainAttachmentsTestScenario", make_shared<TerrainAttachmentsTestScenario>()); },
+            [](){ return make_pair("IPTestsScenario", make_shared<IPTestsScenario>()); },
+            [](){ return make_pair("EasingTestScenario", make_shared<EasingTestScenario>()); },
+            [](){ return make_pair("TerrainTestScenario", make_shared<TerrainTestScenario>()); },
+        };
+        
+        _index = 0;
+        loadScenario();
     }
+    
+    //
+    // command+shift+[left or right bracket] to switch through test scenarios
+    //
+
+    void keyDown( ci::app::KeyEvent event ) override {
+        if (event.isShiftDown() && event.isMetaDown()) {
+            switch(event.getCode()) {
+                case ci::app::KeyEvent::KEY_LEFTBRACKET:
+                    if (_index > 0) {
+                        _index--;
+                    } else {
+                        _index = _scenarioFactories.size() - 1;
+                    }
+                    loadScenario();
+                    break;
+                    
+                case ci::app::KeyEvent::KEY_RIGHTBRACKET:
+                    _index = (_index+1) % _scenarioFactories.size();
+                    loadScenario();
+                    break;
+            }
+        }
+    }
+    
+private:
+    
+    void loadScenario() {
+        auto r = _scenarioFactories[_index]();
+        getWindow()->setTitle("Test - " + str(_index) + " (" + r.first + ")");
+        setScenario(r.second);
+    }
+    
+    size_t _index;
+    vector<function<pair<string,ScenarioRef>()>> _scenarioFactories;
 
 };
 
