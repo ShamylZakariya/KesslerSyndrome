94de086032f4a1c6718dcd7be119d53ffb6bd9d1
Shamyl Zakariya <shamyl@gmail.com>
2018-05-04 08:14:59 -0700

    Empty Player file

---
+++ src/game/KesslerSyndrome/entities/Player.cpp
@@ -0,0 +1,9 @@
+//
+//  Player.cpp
+//  KesslerSyndrome
+//
+//  Created by Shamyl Zakariya on 5/4/18.
+//  Copyright © 2018 Shamyl Zakariya. All rights reserved.
+//
+
+#include "Player.hpp"
---
+++ src/game/KesslerSyndrome/entities/Player.hpp
@@ -0,0 +1,14 @@
+//
+//  Player.hpp
+//  KesslerSyndrome
+//
+//  Created by Shamyl Zakariya on 5/4/18.
+//  Copyright © 2018 Shamyl Zakariya. All rights reserved.
+//
+
+#ifndef Player_hpp
+#define Player_hpp
+
+#include <stdio.h>
+
+#endif /* Player_hpp */
